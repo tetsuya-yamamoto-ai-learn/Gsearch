b66f5589093cf5f5e6650283a65b43158ac25a81
tetsuya-yamamoto-ai-learn <tetsuya.yamamoto.ai.study@gmail.ocm>
2019-09-24 13:14:15 +0900

    ワインに対する検索結果を表示できるようにした
    - hit_per_pageを設けて5件だけ表示できるようにした
    - 出力結果をfor文を使って表示するようにした

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import requests
+
+
 def main():
-    pass
+
+    # 入力
+    payload = {
+        'keyid' : 'e1baa9e16a360a6cb32e16f863fe45a2',   #基本APIキーは公開厳禁(push厳禁)
+        'freeword': 'ワイン',
+        'hit_per_page' : 5
+    }
+    url = 'https://api.gnavi.co.jp/RestSearchAPI/v3/'
+
+    # 計算（情報の取得）
+    response = requests.get(url, params=payload)
+    search_results = response.json()['rest']
+
+    # 出力
+    for result in search_results:    # csv(comma separated value)形式で表示
+        print(f'{result["name"]}, {result["url"]}, {result["access"]["line"]}{result["access"]["station"]}')
 
 
 if __name__ == '__main__':
